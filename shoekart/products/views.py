ea5a5fea817bf684a9433a9b9c1f68d75294a20f
unknown <amalharipdm@gmail.com>
2023-06-01 19:05:14 +0530

    Change product size

--- shoekart/products/views.py
+++ shoekart/products/views.py
@@ -69,28 +69,39 @@ def product_detail(request,id):
     product_images = ProductImages.objects.filter(name=products)
     colors = ProductImages.objects.filter(name=products).values_list('colors__color_name', flat=True).distinct()
     selected_color = request.GET.get('color')
+    selected_varient = request.GET.get('size')
     # if selected_color:
     #     product_images = product_images.filter(colors__color_name=selected_color)
     varients = ProductVarient.objects.filter(name = products)
 
     for i in range(0,len(product_images)):
         product_images[i].str_colors = str(product_images[i].colors)
+    
+    str_varients = []
+    for i in range(0, len(varients)):
+        str_varients.append(str(varients[i])) 
 
     if not selected_color:
         selected_color = product_images[0].str_colors
+
+    if not selected_varient:
+        selected_varient = str_varients[0]
+    print(str_varients[0], selected_varient, str_varients)
     
     context = {
         'products':products, 
         'product_images':product_images,
         'varients':varients,
+        'str_varients':str_varients,
         'colors' : colors,
         # 'selected_color': selected_color,
         # 'my_color': 'Blue'
-        'selected_color': selected_color
+        'selected_color': selected_color,
+        'selected_varient': selected_varient,
         }
-    print(selected_color, "',    '" + str(type(product_images[0].str_colors)) + "'")
+    # print(selected_color, "',    '" + str(type(product_images[0].str_colors)) + "'")
     # print(product_images)
-    print(product_images[1].colors)
+    # print(product_images[1].colors)
     return render(request,'products/product_detail.html', context)
 
 # def product_detail(request,id):
